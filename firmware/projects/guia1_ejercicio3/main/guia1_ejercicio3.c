dfab80841899acf639a883a788218b28fb715cad
Lautaro <141234780+LautaroGZ@users.noreply.github.com>
2026-03-11 16:28:05 -0300

    Commit final clase 11/3/2026
    Se terminaron los ejercicios 1 y 2 de la guía 1. También se dejo definida la estructura para seguir trabajando con la consigna 3

---
+++ firmware/projects/guia1_ejercicio3/main/guia1_ejercicio3.c
@@ -0,0 +1,38 @@
+/*! @mainpage Blinking switch
+ *
+ * \section genDesc General Description
+ *
+ * This example makes LED_1 and LED_2 blink if SWITCH_1 or SWITCH_2 are pressed.
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
+/*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 1000
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+struct leds:
+{
+	uint8_t mode;
+	uint8_t n_led;
+	uint8_t n_ciclos;
+	uint8_t periodo;
+} my_leds;
